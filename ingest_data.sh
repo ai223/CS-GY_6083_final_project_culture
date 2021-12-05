1e015e2f3b4c0038a5bea2e69e20ed5444061197
Aleks Itskovich <31596660+ai223@users.noreply.github.com>
2021-12-05 17:37:58 -0500

    Update ingest_data.sh

--- ingest_data.sh
+++ ingest_data.sh
@@ -6,3 +6,6 @@ cat ./filmdirector.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.filmdirect
 cat ./has_director_Film.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.has_director_Film FROM STDIN CSV HEADER"
 cat ./FilmScreenWriter.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.filmscreenwriter FROM STDIN CSV HEADER"
 cat ./has_screenwriter.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.has_screenwriter FROM STDIN CSV HEADER"
+cat ./FilmActor.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.filmactor FROM STDIN CSV HEADER"
+cat ./has_actor.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.has_actor FROM STDIN CSV HEADER"
+cat ./Location.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.location FROM STDIN CSV HEADER"
