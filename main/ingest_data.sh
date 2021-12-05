425c30b05dc89b0dcd51bf43d51d93d94643ab9f
Aleks Itskovich <31596660+ai223@users.noreply.github.com>
2021-12-05 16:49:41 -0500

    Delete ingest_data.sh
    windows dos file endings messing things up

--- main/ingest_data.sh
+++
@@ -1,2 +0,0 @@
-cat ./filmdirector.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.filmdirector FROM STDIN CSV HEADER"
-cat ./has_director_Film.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.has_director_Film FROM STDIN CSV HEADER"
