810f3462a337dae0fd1d11a945c423872f3d13b8
Aleks Itskovich <31596660+ai223@users.noreply.github.com>
2021-12-05 16:57:18 -0500

    adding-ingest-file-from-remote-version
    and updating project instructions

---
+++ ingest_data.sh
@@ -0,0 +1,8 @@
+echo "Refreshing tables..."
+psql -d ai1221_db -f /home/ai1221/main/create_db.sql
+
+echo "Loading all data..."
+cat ./filmdirector.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.filmdirector FROM STDIN CSV HEADER"
+cat ./has_director_Film.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.has_director_Film FROM STDIN CSV HEADER"
+cat ./FilmScreenWriter.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.filmscreenwriter FROM STDIN CSV HEADER"
+cat ./has_screenwriter.csv | psql -U ai1221 -d ai1221_db -c "COPY culture.has_screenwriter FROM STDIN CSV HEADER"
