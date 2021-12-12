ac9d8a3b44a99c4496f1254c014ffd15735bc6c2
Daniel Gray <calcmaster7@gmail.com>
2021-12-12 18:35:49 -0500

    null handlers not working

--- main/culture_db.py
+++ main/culture_db.py
@@ -244,8 +244,7 @@ if director and genre and borough3:
 		AND F.genre = '{genre}'"""
 
 	try:
-		#directorboroughgenre = query_db(sql_director_and_borough_and_genre)
-		directorboroughgenre = query_db(sql_director_and_borough_and_genre).loc[0]['query5']
+		directorboroughgenre = query_db(sql_director_and_borough_and_genre)
 		if directorboroughgenre is None:
 			st.write("There are no films in our database with that Director/Genre playing in the Borough you selected :(")
 		else:
